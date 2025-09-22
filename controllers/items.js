5a68364228914e6da3810e46b2eb1f6ad1147e6c
ali <ali.hassan.salman.2000@gmail.com>
2025-09-22 16:44:39 +0300

    add about page and its route

--- controllers/items.js
+++ controllers/items.js
@@ -3,10 +3,17 @@ const router = express.Router();
 
 const Item = require('../models/items');
 /* ----------------------------------- ROUTES ------------------------------------------- */
-
+// Default page for users an admins
 router.get('/', (req, res) => {
   res.render('items/index.ejs');
 });
+
+// render about page for users and admins
+router.get('/about.ejs', (req, res) => {
+  res.render('items/about.ejs');
+});
+
+
 
 /* ----------------------------------- EXPORT ------------------------------------------- */
 module.exports = router;
