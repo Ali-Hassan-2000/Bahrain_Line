4339154b2d9a77414c042027cc2cf105206349a3
ali <ali.hassan.salman.2000@gmail.com>
2025-09-22 11:24:29 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,72 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
+
+// Middlewares
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+
+// Controllers
+const authController = require('./controllers/auth.js');
+// add items, categories and user controllers here
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// MIDDLEWARE
+//
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride('_method'));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+app.use(passUserToView);
+
+// Default page for users and admins
+/*
+app.get('/items', (req, res) => {
+  res.render('index.ejs');
+});
+ */
+
+// mount listings controller
+app.use('/auth', authController);
+// add user and items and category
+// app.use('/items', listingsController);
+
+/* ----------------------------------- ROUTES --------------------------------------- */
+// PROTECTED
+/*
+app.use('/users', isSignedIn, usersController);
+*/
+
+/* ----------------------------------- TCP --------------------------------------- */
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
